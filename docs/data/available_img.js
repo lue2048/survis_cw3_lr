e1957eb0f3711b5d030597cb2e8e92c8a7db2cfd
lue2048 <1304777121@qq.com>
2025-05-06 04:29:03 +0100

    Add docs folder for Github Pages deployment

---
+++ docs/data/available_img.js
@@ -0,0 +1,15 @@
+define({
+    availableImg: [
+      "pack2024_llm_aes",
+      "wang2024_llm_validity",
+      "mizumoto2023_gpt_toefl11",
+      "song2024_openllm_aes_aer",
+      "cai2025_rank_score",
+      "yavuz2025_llm_rubric",
+      "su2025_essayjudge_benchmark",
+      "xu2024_slr_aes",
+      "lee2024_cot_scoring",
+      "qinthara2025_reliability_aes"
+    ]
+  });
+  
